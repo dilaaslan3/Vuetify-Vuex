f93c05d3efad79d5402c7dc85c10cf7f1082583d
dilaaslan3 <dilaaslan0144@gmail.com>
2019-01-28 21:44:27 +0300

    CreateMeetup componentinde form elemanları eklendi

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,13 +1,93 @@
 <template>
-<div>
-  <p>Create meetup page</p>
-</div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1>Create meetup page</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <v-form>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="title"
+                label="Title"
+                v-model="title"
+                id="title"
+                required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="location"
+                label="Location"
+                v-model="location"
+                id="location"
+                required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="imageUrl"
+                label="Image Url"
+                id="image-url"
+                v-model="imageUrl"
+                required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" width="100%">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-textarea
+                name="description"
+                label="Description"
+                v-model="description"
+                id="description"
+                required
+                multi-line
+              ></v-textarea>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary" :disabled="!formIsValid">Create Meetup</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'CreateMeetup'
+export default {
+  data () {
+    return {
+      title: '',
+      location: '',
+      imageUrl: '',
+      description: ''
+    }
+  },
+  computed: {
+    formIsValid () {
+      return this.title !== '' &&
+             this.location !== '' &&
+             this.description !== '' &&
+             this.imageUrl !== ''
+    }
   }
+}
 </script>
 
 <style scoped>
