85b05f80441cc8d578abc9eb35e2eeac7878917d
dilaaslan3 <dilaaslan0144@gmail.com>
2019-02-04 01:21:16 +0300

    firebase bağlandı, signup page oluşturuldu

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,12 +1,84 @@
 <template>
-  <div>
-    <p>Sign up page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form @submit.prevent="onSignUp">
+                <v-layout>
+                  <v-flex xs12 sm6 offset-sm3>
+                    <v-text-field
+                      name="email"
+                      label="Mail"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required>
+                    </v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout>
+                  <v-flex xs12 sm6 offset-sm3>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required>
+                    </v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout>
+                  <v-flex xs12 sm6 offset-sm3>
+                    <v-text-field
+                      name="password"
+                      label="Confirm Password"
+                      id="confirmPassword"
+                      v-model="confirmPassword"
+                      type="password"
+                      :rules="[comparePasswords]">
+                    </v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout>
+                  <v-flex xs12 sm6 offset-sm3>
+                    <v-btn type="submit">Sign in</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'Signup'
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    comparePasswords () {
+      return this.password !== this.confirmPassword ? 'Passwords do not match' : true
+    }
+  },
+  methods: {
+    onSignUp () {
+      console.log({
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+      })
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,11 +4,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  created () {
+    // adding firebase
+    firebase.initializeApp({
+      apiKey: '',
+      authDomain: 'test-project-f38b9.firebaseapp.com',
+      databaseURL: 'https://test-project-f38b9.firebaseio.com',
+      projectId: 'test-project-f38b9',
+      storageBucket: 'test-project-f38b9.appspot.com'
+    })
+  },
   render: h => h(App)
 }).$mount('#app')
